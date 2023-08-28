5c5c6a62dc8937dff197a372d798e0eb2bc69f73
Luis Valladares <ls.valladares7@gmail.com>
2023-08-27 21:35:21 -0600

    Fix bug #2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://luisvalladaresc.github.io/',
+  base: 'https://luisvalladaresc.github.io/curso-basico-github-actions/',
   plugins: [react()],
 })
